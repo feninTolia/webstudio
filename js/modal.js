3073802d47e57a5f636b24a8a352b8f84dede5ce
feninTolia <fenintolja@gmail.com>
2022-11-20 16:47:38 +0200

    styles fixes

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,43 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function removeListeners() {
+    document.removeEventListener('keydown', onEscapeInModalClick);
+    refs.closeModalBtn.removeEventListener('click', closeModal);
+    refs.modal.removeEventListener('click', onModalBackdropClick);
   }
+
+  function openModal() {
+    refs.modal.classList.remove('is-hidden');
+    document.addEventListener('keydown', onEscapeInModalClick);
+    refs.closeModalBtn.addEventListener('click', closeModal);
+    refs.modal.addEventListener('click', onModalBackdropClick);
+  }
+
+  function closeModal() {
+    refs.modal.classList.add('is-hidden');
+    removeListeners();
+  }
+
+  function onModalBackdropClick(e) {
+    if (e.target.classList.value !== 'backdrop') {
+      return;
+    }
+    refs.modal.classList.toggle('is-hidden');
+    removeListeners();
+  }
+
+  const onEscapeInModalClick = (e) => {
+    if (e.key !== 'Escape') {
+      return;
+    }
+    refs.modal.classList.toggle('is-hidden');
+    removeListeners();
+  };
+
+  refs.openModalBtn.addEventListener('click', openModal);
 })();
